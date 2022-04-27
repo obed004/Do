fbef25dd6262e8d209a4f863829680decce82e0e
obed004 <100470457+obed004@users.noreply.github.com>
2022-04-27 19:10:19 +0100

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+//1st assignment from wednesday()
+var parent = document.getElementsByClassName(".parent");
+parent.innerHTML = "hello";
+console.log(parent);//end of first assignment
+console.dir(document)
+console.log(document.URL)
+document.title = 123
+var teem =document.getElementById("teem");
+console.log(teem);
+//2nd assignment
+//var second = document.getElementsByClassName("second")
+//console.log(second);
+
+var team =document.getElementById("second");
+team.style.display = "none"
+console.log(team);
